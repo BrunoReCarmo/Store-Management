85445cf6aa5b2455b81cfd648659d689d3c65b39
Bruno Carmo <bruno.reis.carmo@gmail.com>
2024-06-01 21:43:46 -0300

    Adicionar arquivos de projeto.

---
+++ LojaNet.DAL/ClienteDAL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LojaNet.model;
+
+//Model 
+using LojaNet.models;
+
+namespace LojaNet.DAL
+{
+    //Classe ":" implemente Interface Cliente Dados
+    public class ClienteDAL : IClienteDados
+    {
+        public void Alterar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Incluir(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorEmail(string Email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorId(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LojaNet.model/IClienteDados.cs
@@ -0,0 +1,20 @@
+﻿using LojaNet.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Interface p/ métodos models - (Padrão I{Nome do Objeto})
+namespace LojaNet.models
+{
+    public interface IClienteDados
+    {
+        void Incluir(Cliente cliente);
+        void Alterar(Cliente cliente);
+        void Excluir(string Id);
+        List<Cliente> ObterTodos();
+        Cliente ObterPorId(string  Id);
+        Cliente ObterPorEmail(string Email);
+    }
+}
