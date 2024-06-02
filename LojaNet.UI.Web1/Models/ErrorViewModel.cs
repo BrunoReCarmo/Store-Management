8351dba7eaf378e40ef947f9fa02e9302e89dab8
Bruno Carmo <bruno.reis.carmo@gmail.com>
2024-06-01 23:49:03 -0300

    UI WEB View

---
+++ LojaNet.UI.Web1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LojaNet.UI.Web1.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
