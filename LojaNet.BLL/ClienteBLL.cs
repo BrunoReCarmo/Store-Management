2b3e1a9f7bf4c658ddf1d52e7f4794353723423e
Bruno Carmo <bruno.reis.carmo@gmail.com>
2024-06-01 22:02:49 -0300

    BLL project created.

---
+++ LojaNet.BLL/ClienteBLL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Project
+using LojaNet.models;
+using LojaNet.DAL;
+using LojaNet.model;
+
+
+namespace LojaNet.BLL
+{
+    public class ClienteBLL : IClienteDados
+    {
+        public void Alterar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Incluir(Cliente cliente)
+        {
+            if(string.IsNullOrEmpty(cliente.Nome))
+            {
+                throw new ApplicationException("O nome deve ser preenchido");
+            }
+            if (string.IsNullOrEmpty(cliente.Id))
+            {
+                //Guid cria o ID se não houver existente
+                cliente.Id=Guid.NewGuid().ToString();
+            }
+
+            var dal = new ClienteDAL();
+            dal.Incluir(cliente);
+        }
+
+        public Cliente ObterPorEmail(string Email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorId(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
